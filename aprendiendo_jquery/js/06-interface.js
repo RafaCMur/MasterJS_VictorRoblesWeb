4daca49318d427acd34c6958c16a08709f012489
RafaCMur <racabanil@alumnos.unex.es>
2023-09-06 15:35:02 +0200

    Empezando CSS del proyecto en JS

--- aprendiendo_jquery/js/06-interface.js
+++ aprendiendo_jquery/js/06-interface.js
@@ -1,12 +1,46 @@
 $(document).ready(function(){
+    let liSeleccionable = $('.lista-seleccionable')
     //Mover elemento por la pagina
     $('.elemento').draggable()
-
 
     // Redimensionar
     $('.elemento').resizable()
 
-
     // Seleccionar elementos
-    $('.lista-seleccionable').selectable ()
+    //liSeleccionable.selectable ()
+
+    // Reordenar elementos de la lista dragging them
+    liSeleccionable.sortable ({
+        update: function (event, ui) {
+            console.log("Ha cambiado la lista");
+        }
+    })
+
+    $('#elemento-movido').draggable()
+    
+    $('#area').droppable({
+        drop: function() {
+            console.log('Has soltado algo dentro de la pantalla')
+        }
+    })
+
+
+    // Efectos
+    $('#mostrar').click(function () {
+        $('.caja-efectos').toggle('fade')
+    })
+
+    //Tooltip
+    $(document).tooltip()
+
+    //Dialog
+    $('#lanzar-popup').click(function () {
+        $('#popup').dialog()
+    })
+
+    //Datepicker
+    $('#calendario').datepicker()
+
+    //Tabs
+    $('#pestanas').tabs()
 })
